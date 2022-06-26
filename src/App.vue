06ad5ff3c612e5c3fdccef13924a7d6ae95df074
YanMyoAung <yanmyoaung.mdy1500@gmail.com>
2022-06-26 22:21:35 +0630

    create email & password form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUp/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from "./components/SignUp.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignUp
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  background-color: #eee;
+  margin:0
 }
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,59 @@
+<template>
+  <form>
+    <label for="">Email</label>
+    <input type="email" required v-model="email">
+
+    <label for="">password</label>
+    <input type="password" v-model="password">
+    
+    <label for="">Roles: </label>
+    <select v-model="role">
+        <option value="developer">Web Developer</option>
+        <option value="designer">Web Designer</option>
+    </select>
+  </form>
+  <p>email- {{email}}</p>
+  <p>password- {{password}}</p>
+  <p>role- {{role}}</p>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:"yma@gmail.com",
+            password:"123456",
+            role:"developer"
+        }
+    }    
+}
+</script>
+
+<style>
+    form{
+        max-width: 420px;
+        margin: 30px auto;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label{
+        color: #aaa;
+        display: inline-block;
+        margin: 25px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input,select{
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+</style>
