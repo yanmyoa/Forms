d550ab72b35c4c13f9b27c6255898867580ceb18
YanMyoAung <yanmyoaung.mdy1500@gmail.com>
2022-06-29 21:02:58 +0630

    Vuejs Login Logout forms

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,20 +1,49 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <label for="">Email</label>
     <input type="email" required v-model="email">
 
     <label for="">password</label>
     <input type="password" v-model="password">
+    <p v-if="errorMsg" class="error">{{errorMsg}}</p>
     
     <label for="">Roles: </label>
     <select v-model="role">
         <option value="developer">Web Developer</option>
         <option value="designer">Web Designer</option>
     </select>
+
+    <div>
+        <label for="">Skills</label>
+        <input type="text" @keyup.alt="addSkill" v-model="skill">
+    </div>
+
+    <div v-for="skill in skills" :key="skill">
+        <p>{{skill}}
+            <span class="cross" @click="deleteSkill(skill)">
+                &#x2716;
+            </span>
+        </p>
+    </div>
+
+    <!-- single checkbox -->
+    <div>
+        <input type="checkbox" v-model="accept">
+        <label for="">Accept Terms and condition</label>
+    </div>
+
+    <div class="align">
+        <button class="create">Create Account</button>
+    </div>
+
   </form>
+
   <p>email- {{email}}</p>
   <p>password- {{password}}</p>
   <p>role- {{role}}</p>
+  <p>accept - {{accept}}</p>
+  <!-- <p>names - {{names}}</p> -->
+
 </template>
 
 <script>
@@ -22,10 +51,33 @@ export default {
     data(){
         return{
             email:"yma@gmail.com",
-            password:"123456",
-            role:"developer"
+            password:"",
+            role:"developer",
+            accept:false,
+            // names:[]
+            skills:[],
+            skill:"",
+            errorMsg:""
         }
-    }    
+    },
+    methods:{
+        addSkill(e){
+            if(e.key==="," && this.skill){
+                this.skills.push(this.skill);
+                this.skill=""
+            }
+        },
+        deleteSkill(skill){
+            this.skills=this.skills.filter(loopSkill=>{
+                return loopSkill!=skill;
+            })
+        },
+        submit(){
+            if(this.password.length<8){
+                this.errorMsg="Password Must me at least 8 characters"
+            }
+        }
+    } 
 }
 </script>
 
@@ -55,6 +107,29 @@ export default {
         border: none;
         border-bottom: 1px solid #ddd;
         color: #555;
+    }
+    input[type='checkbox']{
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
+    }
+    .cross{
+        cursor: pointer;
+        color: red;
+    }
+    .create{
+        background-color: royalblue;
+        padding: 8px;
+        color: white;
+        border-radius: 10px;
+    }
+    .align{
+        text-align: center;
+    }
+    .error{
+        color: crimson;
     }
 </style>
 
